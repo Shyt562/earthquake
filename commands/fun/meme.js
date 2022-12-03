3f775c53c84fc1f91c6c0afecaaab48f6e0d5db6
Shyt562 <98867424+Shyt562@users.noreply.github.com>
2022-12-03 09:38:42 +0800

    Update meme.js

--- commands/fun/meme.js
+++ commands/fun/meme.js
@@ -4,7 +4,7 @@ const randomPuppy = require("random-puppy");
 module.exports = {
     name: "meme",
     category: "fun",
-    description: "Sends an epic meme",
+    description: "發送一個模因",
     run: async (client, message, args) => {
         const subReddits = ["dankmeme", "meme", "me_irl"];
         const random = subReddits[Math.floor(Math.random() * subReddits.length)];
