472de1aba67947d3e070d4d9a6513d17bc20d119
Shyt562 <98867424+Shyt562@users.noreply.github.com>
2022-12-03 09:59:58 +0800

    Update ban.js

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -5,34 +5,34 @@ const { promptMessage } = require("../../functions.js");
 module.exports = {
     name: "ban",
     category: "moderation",
-    description: "bans the member",
-    usage: "<id | mention>",
+    description: "禁止會員",
+    usage: "<id | 標記>",
     run: async (client, message, args) => {
-        const logChannel = message.guild.channels.find(c => c.name === "logs") || message.channel;
+        const logChannel = message.guild.channels.find(c => c.name === "日誌") || message.channel;
 
         if (message.deletable) message.delete();
 
         // No args
         if (!args[0]) {
-            return message.reply("Please provide a person to ban.")
+            return message.reply("請提供要禁止的人。")
                 .then(m => m.delete(5000));
         }
 
         // No reason
         if (!args[1]) {
-            return message.reply("Please provide a reason to ban.")
+            return message.reply("請提供禁止的理由。")
                 .then(m => m.delete(5000));
         }
 
         // No author permissions
-        if (!message.member.hasPermission("BAN_MEMBERS")) {
-            return message.reply("❌ You do not have permissions to ban members. Please contact a staff member")
+        if (!message.member.hasPermission("成員")) {
+            return message.reply("❌ 您無權禁止成員。請聯繫其他管理員")
                 .then(m => m.delete(5000));
         
         }
         // No bot permissions
-        if (!message.guild.me.hasPermission("BAN_MEMBERS")) {
-            return message.reply("❌ I do not have permissions to ban members. Please contact a staff member")
+        if (!message.guild.me.hasPermission("成員")) {
+            return message.reply("❌ 我沒有禁止成員的權限。請聯繫管理員")
                 .then(m => m.delete(5000));
         }
 
@@ -40,19 +40,19 @@ module.exports = {
 
         // No member found
         if (!toBan) {
-            return message.reply("Couldn't find that member, try again")
+            return message.reply("找不到該成員，請重試")
                 .then(m => m.delete(5000));
         }
 
         // Can't ban urself
         if (toBan.id === message.author.id) {
-            return message.reply("You can't ban yourself...")
+            return message.reply("你不能禁止自己...")
                 .then(m => m.delete(5000));
         }
 
         // Check if the user's banable
         if (!toBan.bannable) {
-            return message.reply("I can't ban that person due to role hierarchy, I suppose.")
+            return message.reply("我想，由於角色層次的關係，我不能禁止那個人。")
                 .then(m => m.delete(5000));
         }
         
@@ -61,14 +61,14 @@ module.exports = {
             .setThumbnail(toBan.user.displayAvatarURL)
             .setFooter(message.member.displayName, message.author.displayAvatarURL)
             .setTimestamp()
-            .setDescription(stripIndents`**- baned member:** ${toBan} (${toBan.id})
-            **- baned by:** ${message.member} (${message.member.id})
-            **- Reason:** ${args.slice(1).join(" ")}`);
+            .setDescription(stripIndents`**- 被禁會員:** ${toBan} (${toBan.id})
+            **- 被禁止:** ${message.member} (${message.member.id})
+            **- 原因:** ${args.slice(1).join(" ")}`);
 
         const promptEmbed = new RichEmbed()
             .setColor("GREEN")
-            .setAuthor(`This verification becomes invalid after 30s.`)
-            .setDescription(`Do you want to ban ${toBan}?`)
+            .setAuthor(`此驗證在 30 秒後失效。`)
+            .setDescription(`你想禁止嗎 ${toBan}?`)
 
         // Send the message
         await message.channel.send(promptEmbed).then(async msg => {
@@ -81,14 +81,14 @@ module.exports = {
 
                 toBan.ban(args.slice(1).join(" "))
                     .catch(err => {
-                        if (err) return message.channel.send(`Well.... the ban didn't work out. Here's the error ${err}`)
+                        if (err) return message.channel.send(`好吧……禁令沒有奏效。這是錯誤 ${err}`)
                     });
 
                 logChannel.send(embed);
             } else if (emoji === "❌") {
                 msg.delete();
 
-                message.reply(`ban canceled.`)
+                message.reply(`禁令取消。`)
                     .then(m => m.delete(10000));
             }
         });
