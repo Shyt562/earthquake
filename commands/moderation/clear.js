f00d8403354270c5bdf53243ea2ea17ff6eeb71b
Shyt562 <98867424+Shyt562@users.noreply.github.com>
2022-12-03 10:02:30 +0800

    Update clear.js

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -1,26 +1,26 @@
 module.exports = {
     name: "clear",
-    aliases: ["purge", "nuke"],
+    aliases: ["清除", "核武器"],
     category: "moderation",
-    description: "Clears the chat",
+    description: "清空聊天",
     run: async (client, message, args) => {
         if (message.deletable) {
             message.delete();
         }
     
         // Member doesn't have permissions
-        if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-            return message.reply("You can't delete messages....").then(m => m.delete(5000));
+        if (!message.member.hasPermission("管理消息")) {
+            return message.reply("你不能刪除消息....").then(m => m.delete(5000));
         }
 
         // Check if args[0] is a number
         if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
-            return message.reply("Yeah.... That's not a numer? I also can't delete 0 messages by the way.").then(m => m.delete(5000));
+            return message.reply("是啊……那不是數字嗎？順便說一下，我也不能刪除 0 條消息。").then(m => m.delete(5000));
         }
 
         // Maybe the bot can't delete messages
-        if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) {
-            return message.reply("Sorryy... I can't delete messages.").then(m => m.delete(5000));
+        if (!message.guild.me.hasPermission("管理消息")) {
+            return message.reply("抱歉...我無法刪除消息。").then(m => m.delete(5000));
         }
 
         let deleteAmount;
@@ -32,7 +32,7 @@ module.exports = {
         }
 
         message.channel.bulkDelete(deleteAmount, true)
-            .then(deleted => message.channel.send(`I deleted \`${deleted.size}\` messages.`))
-            .catch(err => message.reply(`Something went wrong... ${err}`));
+            .then(deleted => message.channel.send(`我刪除了 \`${deleted.size}\` 消息。`))
+            .catch(err => message.reply(`出問題了... ${err}`));
     }
 }
